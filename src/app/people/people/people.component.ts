34b15faefd56ccf0a4bd4208f03159eeae0a6780
Yury Kuzmich <yurakuzmich@gmail.com>
2024-04-17 18:15:19 +0500

    feat: people loader

--- src/app/people/people/people.component.ts
+++ src/app/people/people/people.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SWHero } from '../../models/people.model';
 import { PeopleService } from '../people.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html',
   styleUrl: './people.component.scss'
 })
-export class PeopleComponent implements OnInit {
+export class PeopleComponent implements OnInit, OnDestroy {
 
   public title = 'Star Wars Heroes';
   public people: SWHero[] = [];
+  public loading = true;
+  public error = false;
+  private peopleSubscription!: Subscription;
+
   constructor(private peopleService: PeopleService) { }
 
   ngOnInit(): void {
-    this.peopleService.getPeople().subscribe(data => {
+    this.peopleSubscription = this.peopleService.getPeople().subscribe(data => {
       this.people = data.results;
+      this.loading = false;
     })
+  }
+
+  ngOnDestroy(): void {
+    this.peopleSubscription.unsubscribe();
   }
 }
