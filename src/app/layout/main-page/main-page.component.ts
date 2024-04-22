140adb3624969e3c68e0ed8223d9040301f174ef
Yury Kuzmich <yurakuzmich@gmail.com>
2024-04-23 00:17:42 +0500

    feat: images and animation

--- src/app/layout/main-page/main-page.component.ts
+++ src/app/layout/main-page/main-page.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrl: './main-page.component.scss',
+  animations: [
+    trigger('underlay', [
+      state(
+        'underlay-up',
+        style({
+          transform: 'translateY(0)',
+        })
+      ),
+      state(
+        'underlay-down',
+        style({
+          transform: 'translateY(120%)',
+        }),
+      ),
+      transition('* => *', [animate('1s ease-out')]),
+    ]),
+    trigger('lightning-bulb', [
+      state(
+        'small',
+        style({
+          transform: 'scale(1)',
+        }),
+      ),
+      state(
+        'large',
+        style({
+          transform: 'scale(10)',
+        }),
+      ),
+      transition('large => small', [animate('1s ease-out')]),
+    ]),
+    trigger('lightning-position', [
+      state(
+        'left',
+        style({
+          transform: 'translateX(-20px)',
+        })
+      ),
+      state(
+        'right',
+        style({
+          transform: 'translateX(20px)'
+        }),
+      ),
+      transition('* => *', [animate('0.1s')]),
+    ]),
+  ],
 })
 export class MainPageComponent {
+  @ViewChild('yoda') yoda!: ElementRef;
 
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+
+    if(Math.abs(event.clientX - this.currentMouseX) > 100 || Math.abs(event.clientY - this.currentMouseY) > 100) {
+      this.currentMouseX = event.clientX;
+      this.currentMouseY = event.clientY;
+      this.lightningPosition = this.lightningPosition === 'left' ? 'right' : 'left';
+    }
+  }
+
+  public underlayState = 'underlay-up';
+  public lightningState = 'large';
+  public lightningPosition = 'left';
+   currentMouseX = 0;
+  private currentMouseY = 0;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.underlayState = 'underlay-down';
+    }, 2000);
+    setTimeout(() => {
+      this.lightningState = 'small';
+    }, 1000)
+  }
 }
--- src/app/layout/menu-item/menu-item.component.ts
+++ src/app/layout/menu-item/menu-item.component.ts
@@ -1,11 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { MenuItem } from '../../models/menu-item.model';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-menu-item',
   templateUrl: './menu-item.component.html',
-  styleUrl: './menu-item.component.scss'
+  styleUrl: './menu-item.component.scss',
+  animations: [],
 })
 export class MenuItemComponent {
   @Input() props!: MenuItem;
+
+  @ViewChild('menuLink') menuLink!: ElementRef;
+
+  isSelected: boolean = false;
+
+  isActive(): boolean {
+    return this.menuLink.nativeElement.classList.contains('active');
+  }
+
+  onClick() {
+    this.isSelected = !this.isActive();
+    console.log(this.isSelected);
+  }
 }
